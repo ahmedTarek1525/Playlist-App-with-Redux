ca7ceb8ea4ec079b91ddd1a70913b6177d02eb73
Ahmed Tarek <ahmed.t9235@gmail.com>
2024-02-20 23:49:17 +0200

    Deploying the playlists React app

---
+++ src/store/slices/songsSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { reset } from "../actions";
+
+const songsSlice = createSlice({
+    name:'song',
+    initialState: [],
+    reducers: {
+        // 'song' + '/' + 'addsong' = 'song/addsong'
+        // THIS IS WHAT HAPPNES AUTOMATICALLY!
+        addSong( state , action ) {
+            // STATE IS NOT THE BIG STATE OBJECT 
+            // IN THE STORE
+            // IT IS THE PIECE OF STATE MANAGED
+            // BY THIS REDUCER
+          state.push(action.payload);
+        },
+        removeSong( state , action ) {
+           // action.payload === string , the song we want to remove.
+           const index = state.indexOf(action.payload);
+           state.splice(index, 1);
+        }
+    },
+    extraReducers(builder) {
+        builder.addCase( reset , (state, action) => {
+           return [];
+        });
+     }
+ });
+
+ export const {addSong , removeSong} = songsSlice.actions;
+ export const songsReducer = songsSlice.reducer;
